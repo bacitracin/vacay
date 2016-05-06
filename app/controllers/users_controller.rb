f8923b2af931d6fa9991fc08ef09a6c879302a02
bacitracin <tracetrace@gmail.com>
2016-05-05 21:03:07 -0700

    add in User show page, nested Trips under User Routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+before_action :authenticate_user!, except: [:index, :show]
 
-  
+  def show
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users 
+
+  resources :users do
+    resources :trips
+  end
+
+
   resources :trips
   resources :destinations
 
