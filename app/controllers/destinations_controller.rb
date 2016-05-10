7a4078f26046cd510c70e7b69d8b80610090cda4
bacitracin <tracetrace@gmail.com>
2016-05-10 12:31:06 -0700

    add New Destination form and views

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -7,44 +7,46 @@ class DestinationsController < ApplicationController
 
   def new
     @destination = Destination.new
-    @trip = @destination.trips.build(:user_id => current_user.id, :destination_id => @destination.id)
+    #@trip = @destination.trips.build(:user_id => current_user.id, :destination_id => @destination.id)
   end
 
-  # def create
-  #   @destination = Destination.new(destination_params)
-  #   if @destination.save
-  #     redirect_to @destination
-  #   else
-  #     render :new
-  #   end
-  # end
+   def create
+     @destination = Destination.find_or_create_by(destination_params)
+     if @destination.save
+       redirect_to @destination
+     else
+       render :new
+     end
+   end
 
-  # def show
-  #    @destination = Destination.find(params[:id])
-  # end
+   def show
+      @destination = Destination.find(params[:id])
+   end
 
-  # def edit
-  #   @destination = Destination.find(params[:id])
-  # end
+   def edit
+     @destination = Destination.find(params[:id])
+   end
 
-  # def update
-  #   if @destination.update(destination_params)
-  #     redirect_to @destination
-  #   else
-  #     render :edit
-  #   end
-  # end
+   def update
+     if @destination.update(destination_params)
+       redirect_to @destination
+     else
+       render :edit
+     end
+   end
 
-   # def destroy
-   #   Destination.find_by_id(params[:id]).destroy
+    def destroy
+      Destination.find_by_id(params[:id]).destroy
 
-   #   redirect_to destinations_path
-   # end
+      redirect_to destinations_path
+    end
 
-   # private
+    private
 
-  # def destination_params
-  #   params.require(:destination).permit(:city, :id, :trips_attributes => [:trip_nickname, :start_date, :end_date, :user_id])
-  # end
+   def destination_params
+     params.require(:destination).permit(:city)
+   end
+   #:trips_attributes => [:trip_nickname, :start_date, :end_date, :user_id])
+   
 
 end
