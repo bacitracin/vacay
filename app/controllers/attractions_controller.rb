caad2216ef7f07d33599dda3de8b4565167ca63f
bacitracin <tracetrace@gmail.com>
2016-05-11 11:40:14 -0700

    works except for the inclusion of [] attraction colelction list object on the trip show page, not the destination page

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -9,18 +9,13 @@ class AttractionsController < ApplicationController
   end
 
   def create
-    @attraction = Attraction.find_or_create_by(attraction_params)
+    @attraction = Attraction.create(attraction_params)
 
     @destination = Destination.find_or_create_by(:city => params[:attraction][:destination][:destination_city])
     @attraction.destination_id = @destination.id
 
     @trip = Trip.find_or_create_by(:trip_nickname=> params[:attraction][:trip][:trip_nickname])
     @attraction.trips << @trip
-    #@attraction.destination = @destination
-    #@destination.attractions << @attraction
-
-   # @trip = Trip.find_or_create_by(:trip_nickname=> params[:attraction][:trip][:trip_nickname])
-   # @attraction.trips << @trip
 
     if @attraction.valid?
       @attraction.save
@@ -30,10 +25,9 @@ class AttractionsController < ApplicationController
     end
   end
 
-
-   def show
-     @attraction = Attraction.find_by_id(params[:id])
-   end
+  def show
+    @attraction = Attraction.find_by_id(params[:id])
+  end
 
   private
 
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -4,12 +4,5 @@ class Destination < ActiveRecord::Base
   has_many :attractions
 
   validates_presence_of :city
-
-   # def trips_attributes=(trips_attributes)
-   #   trips_attributes.each do |i, trip_attributes| 
-   #     self.trips.build(trip_attributes)
-   #   end
-   # end
-
    
 end
