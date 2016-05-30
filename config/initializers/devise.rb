61e3e64007552ff6ed1aaf6d926a7f755ee7235f
bacitracin <tracetrace@gmail.com>
2016-05-29 18:25:10 -0700

    remove FB keys

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -243,8 +243,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
-  config.omniauth :facebook, '1126713894015358', '52f88f65c1f75809ab18d89faef98bab'
-  #config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
