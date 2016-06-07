881f167addda8a49454d86119a10337bd4042a46
bacitracin <tracetrace@gmail.com>
2016-06-07 14:17:26 -0700

    individual trip is loading, but need to extract from the entire layout and have it show up underneath the trip itself

--- app/assets/javascripts/vacay.js
+++ app/assets/javascripts/vacay.js
@@ -1,12 +1,10 @@
 // Click on get more info link on the user/trip index page, populates with trips?s
 
-$(function () {
-  $(".js-more").on('click', function(event) {
+$(function(){
+  $(".js-more").on("click", function(event){
     event.preventDefault();
-    var id = $(this).data("id");
-    $.get("/trips/" + id + ".json", function(data) {
-      // Replace text of body-id div
-      $("div.trip-info").html(data);
-    });
-  });
-});
+    $.get(this.href).success(function(response){
+      $("div.trip-info").html(response)
+    })
+  })
+})
