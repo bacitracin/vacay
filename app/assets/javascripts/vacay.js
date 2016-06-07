c2ecbde0785dee6f2b4fef7a7d02038876cb5a53
bacitracin <tracetrace@gmail.com>
2016-06-07 11:23:07 -0700

    ajax response is the entire page

--- app/assets/javascripts/vacay.js
+++ app/assets/javascripts/vacay.js
@@ -1,12 +1,12 @@
 // Click on get more info link on the user/trip index page, populates with trips?
 
 $(function(){
-  $("a.trip-info").on("click", function(event){
+  $(".js-more").on("click", function(event){
     $.ajax({
     method: "GET",
     url: this.href,
     }).done(function(response){
-    $("div.trip-info")html(response)
+    $("div.trip-info").html(response)
     })
     event.preventDefault();
   })
