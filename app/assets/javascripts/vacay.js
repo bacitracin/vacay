d5d051974ed5ac605bfe776b16de90b4bfb4631a
bacitracin <tracetrace@gmail.com>
2016-06-08 14:54:39 -0700

    append individual Trip info to index page - JS in progress

--- app/assets/javascripts/vacay.js
+++ app/assets/javascripts/vacay.js
@@ -1,40 +1,32 @@
 // Click on get more info link on the user/trip index page, populates with trips?s
 
-// HTML works sort of, you can input the entire page
-//$(function(){
-//  $(".js-more").on("click", function(event){
-//    event.preventDefault();
-//    $.get(this.href).success(function(response){
-//      $("div.trip-info").html(response)
-//    })
-//  })
-//})
-
-
 // JSON
-
 $(function(){
   $(".js-more").on("click", function(event){
     event.preventDefault();
 
-    var tripUrl = (this.href);
-    tripUrl += ".json";
-    var tripInfo = $.get(tripUrl);
-    
-    console.log(tripInfo);
-    alert("i'm working");
-
+    var tripUrl = (this.href += ".json");
+    $.get(tripUrl).success(function(response){
+      $("div.trip-info").append(response["trip"]["destination"]["city"]);
+    })
   })
 })
 
-function Trip (user_id, destination_id, trip_nickname, start_date, end_date){
-  this.user_id = user_id
-  this.destination_id = destination_id
-  this.trip_nickname = trip_nickname
-  this.start_date = start_date
-  this.end_date = end_date
+// Add Trip Info
+function addTripInfoToPage(response){
+  $("div.trip-info").append(response["trip"]["destination"]["city"]);
+};
+
+// Trip Constructor
+function Trip(user_id, city, trip_nickname, start_date, end_date){
+  this.user_id = user_id;
+  this.city = city;
+  this.trip_nickname = trip_nickname;
+  this.start_date = start_date;
+  this.end_date = end_date;
 }
 
+// Placeholder
 Trip.prototype.capitalize = function(){
 
 }
