8536b5412843f1da77f17c440de0e25ce6aa2301
bacitracin <tracyctran@gmail.com>
2016-08-24 10:23:23 -0700

    try jasons suggestion for config line

--- config/application.rb
+++ config/application.rb
@@ -5,10 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Rails.application.config.assets.initialize_on_precompile = false
 module Vacay
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
