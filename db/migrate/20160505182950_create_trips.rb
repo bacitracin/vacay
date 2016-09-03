031c45a59e5502bcab676c8771a6e7e61640afdf
bacitracin <tracyctran@gmail.com>
2016-09-03 13:45:11 -0700

    change migrations string to text, remove migration changing trip date from text to date

--- db/migrate/20160505182950_create_trips.rb
+++ db/migrate/20160505182950_create_trips.rb
@@ -2,10 +2,10 @@ class CreateTrips < ActiveRecord::Migration
   def change
     create_table :trips do |t|
       t.integer :user_id
-      t.string :destination_id
-      t.string :trip_nickname
-      t.string :start_date
-      t.string :end_date
+      t.text :destination_id
+      t.text :trip_nickname
+      t.text :start_date
+      t.text :end_date
       t.integer :trip_attractions
 
       t.timestamps null: false
--- db/migrate/20160526221804_change_data_type_for_trips.rb
+++
@@ -1,8 +0,0 @@
-class ChangeDataTypeForTrips < ActiveRecord::Migration
-  def change
-    change_table :trips do |t|
-        t.change :start_date, :date, 'date USING CAST(start_date AS date)'
-        t.change :end_date, :date, 'date USING CAST(start_date AS date)'
-    end
-  end
-end
