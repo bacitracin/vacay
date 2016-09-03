da77ed03560be507621e6ad27729ddcd89baa2bb
bacitracin <tracyctran@gmail.com>
2016-09-03 13:39:12 -0700

    edit trip data type migration to match postgresql data types

--- db/migrate/20160526221804_change_data_type_for_trips.rb
+++ db/migrate/20160526221804_change_data_type_for_trips.rb
@@ -1,8 +1,8 @@
 class ChangeDataTypeForTrips < ActiveRecord::Migration
   def change
     change_table :trips do |t|
-        t.change :start_date, :date 
-        t.change :end_date, :date
+        t.change :start_date, :date, 'USING start_date::date'
+        t.change :end_date, :date, 'USING end_date::date'
     end
   end
 end
