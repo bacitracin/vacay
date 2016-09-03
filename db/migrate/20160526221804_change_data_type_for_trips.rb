07e0ebf6cc947920143e67e3cdce629a00511d14
bacitracin <tracyctran@gmail.com>
2016-09-03 13:42:54 -0700

    try another syntax

--- db/migrate/20160526221804_change_data_type_for_trips.rb
+++ db/migrate/20160526221804_change_data_type_for_trips.rb
@@ -1,8 +1,8 @@
 class ChangeDataTypeForTrips < ActiveRecord::Migration
   def change
     change_table :trips do |t|
-        t.change :start_date, :date, 'USING start_date::date'
-        t.change :end_date, :date, 'USING end_date::date'
+        t.change :start_date, :date, 'date USING CAST(start_date AS date)'
+        t.change :end_date, :date, 'date USING CAST(start_date AS date)'
     end
   end
 end
